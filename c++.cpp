ca8bf875b4dd120d31a8dabf0f424ecf91c39c45
Ngoc Tai <21T1020663@husc.edu.vn>
2023-08-15 16:08:52 +0700

    bai moi sua ne

--- c++.cpp
+++ c++.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!";
+    cout << "Hello World!\n";
+    cout << "Dit me hoi chu hoi bi cay roi nha !\n";
+    cout << "Me may cu lam cho bo may mo lui mo toi met vai ca lon!";
 }
