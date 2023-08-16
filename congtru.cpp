a55cdc389bb9a0fdeb4e52636582e7ab103be97c
Ngoc Tai <21T1020663@husc.edu.vn>
2023-08-16 09:55:21 +0700

    bai cong tru co ban thoi

---
+++ congtru.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    c = a + b;
+    cout<<"Nhap vao he so cua a va b\n";
+    cout<<"Nhap a: ";
+    cin>>a;
+    cout<<"Nhap b: ";
+    cin>>b;
+    c = a + b;
+    cout<<"Ket qua cua phep tinh a + b la : ";
+    cout<< c;
+    return 0;
+}
